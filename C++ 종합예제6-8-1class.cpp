fae0d34402c9dcb64073e7caf02014cab48b1453
pins-ball <122430370+pins-ball@users.noreply.github.com>
2023-09-06 16:21:39 +0900

    Update C++ 종합예제6-8-1class.cpp

--- C++ 종합예제6-8-1class.cpp
+++ C++ 종합예제6-8-1class.cpp
@@ -16,266 +16,308 @@ using namespace std;
 #define UP 72
 #define scores 1000
 
-class airplane{
+class airplane {
 private:
     int x, y, gx, gy, i, yy, k;
-	int bx[5],by[5],bx2[5],by2[5];
+    int bx[5], by[5], bx2[5], by2[5];
     char face[10];
     char bad1[5][10];
-	char bad2[5][10];
+    char bad2[5][10];
     char bad[10];
     char key_in;
     bool sw1;
-	bool active;
-	char key;
-	int hit;
-	int life;
-	int score;
-	double duration;
-	char getKey() {
+    bool active;
+    char key;
+    int hit;
+    int life;
+    int score;
+    double duration;
+
+    void gotoxy(int x, int y) {
+        COORD pos = { x - 1, y - 1 };
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+    }
+
+    void delay(int x) {
+        Sleep(x);
+    }
+
+    void scr_mov() {
+        gotoxy(x, y);
+        cout << face << endl;
+    }
+
+    void scr_clear() {
+        gotoxy(x, y);
+        cout << "          " << endl;
+    }
+
+    void scr_mov2() {
+        for (i = 0; i < 5; i++) {
+            gotoxy(bx[i], by[i]);
+            cout << bad1[i] << endl;
+        }
+        delay(20);
+
+        for (i = 0; i < 5; i++) {
+            gotoxy(bx2[i], by2[i]);
+            cout << "       " << endl;
+        }
+
+        for (i = 0; i < 5; i++) {
+            bx2[i] = bx[i];
+            by2[i] = by[i];
+        }
+
+        for (i = 0; i < 5; i++) {
+            if (y <= by[i]) {
+                scr_mov();
+                strcpy(bad1[i], "‚óÜ‚Üì‚óÜ");
+            }
+        }
+    }
+
+    void hit_fun() {
+        system("cls");
+        gotoxy(26, 8);
+        cout << "Î™©Ïà®" << life << "ÎÇ®Ïùå" << endl;
+        gotoxy(26, 10);
+        cout << "ÎØ∏ÏÖò ÏÑ±Í≥µ" << endl;
+    }
+
+    void life_fun() {
+        system("cls");
+        gotoxy(26, 8);
+        cout << "ÌòÑÏû¨ Ï†ÅÍµ∞ÏùÄ " << hit << "ÎßàÎ¶¨ ÏÇ¥ÏïÑÏûàÏäµÎãàÎã§.";
+        gotoxy(26, 10);
+        cout << "ÎØ∏ÏÖò Ïã§Ìå®" << endl;
+    }
+
+    void start() {
+        gotoxy(21, 8);
+        cout << "Ï£ºÏù∏Í≥µ Ïù¥Îèô : Ï¢åÏö∞ ÌôîÏÇ¥Ìëú ÌÇ§Î≥¥Îìú" << endl;
+        gotoxy(26, 10);
+        cout << "Î¨¥Í∏∞ Î∞úÏÇ¨ : Ïä§ÌéòÏù¥Ïä§Î∞î" << endl;
+        gotoxy(20, 12);
+        cout << "S ÌÇ§Î•º ÏÑ†ÌÉùÌïòÎ©¥ Í≤åÏûÑÏù¥ ÏãúÏûëÎê©ÎãàÎã§." << endl;
+        cin >> key;
+    }
+
+    void gun_mov() {
+        gotoxy(gx, gy);
+        cout << "‚ñ≤" << endl;
+        gotoxy(gx, gy + 1);
+        cout << "  " << endl;
+        if (gy == 1) {
+            gotoxy(gx, 1);
+            cout << "  " << endl;
+        }
+        scr_crash();
+    }
+
+    void scr_move() {
+        gotoxy(4, 1);
+        cout << "hit : " << hit << " ";
+        gotoxy(16, 1);
+        cout << "life : " << life;
+        if (kbhit()) {
+            key_in = getch();
+            if (key_in == LEFT) {
+                if (x > 2) {
+                    scr_clear();
+                    x--;
+                }
+            }
+            else if (key_in == RIGHT) {
+                if (x < 70) {
+                    scr_clear();
+                    x++;
+                }
+            }
+            else if (key_in == ' ') {
+                if (gy == 0) {
+                    sw1 = true;
+                    gx = x + 3;
+                    gy = 23;
+                }
+            }
+        }
+
+        if (sw1 == true) {
+            gy--;
+            if (gy == 0) {
+                sw1 = false;
+            }
+            else {
+                gun_mov();
+            }
+        }
+
+        if (life == 3 || yy == 24) {
+            active = true;
+        }
+
+        if (yy == 24) {
+            yy = 3;
+        }
+        else {
+            if (k == 10) {
+                k = 0;
+                yy++;
+            }
+            else {
+                k++;
+            }
+        }
+
+        for (i = 0; i < 5; i++) {
+            by[i] = yy;
+        }
+
+        if (active == true) {
+            crash1();
+        }
+
+        scr_mov2();
+    }
+
+    void str_cpy() {
+        char st1[15] = "‚óê-‚ñ≤-‚óë";
+        char st2[10] = "‚óÜ‚Üì‚óÜ";
+        char st3[10] = "      ";
+        strcpy(face, st1);
+        x = 35;
+        y = 22;
+
+        for (int i = 0; i < 5; i++) {
+            strcpy(bad1[i], st2);
+            bx[i] = (i + 1) * 13;
+            by[i] = 3;
+        }
+    }
+
+void scr_crash() {
+    for (int i = 0; i < 5; i++) {
+        if (gx > bx[i] - 1 && gx < bx[i] + 11 && gy == by[i]) {
+			
+            if (strcmp(bad1[i], "       ") != 0) {
+    strcpy(bad1[i], "       "); // Î¨∏ÏûêÏó¥ÏùÑ Î≥µÏÇ¨
+    hit = hit - 1;
+}
+
+        }
+    }
+}
+
+
+
+    void crash1() {
+        for (int i = 0; i < 5; i++) {
+
+            if (x < bx[i] + 6 && x + 8 > bx[i] && y - 1 == by[i]) {
+                if (strcmp(bad1[i], "       ") != 0) {
+                    life = life - 1;
+                    active = false;
+                }
+            }
+        }
+    }
+
+public:
+    airplane() {
+        i = 0;
+        k = 0;
+        gy = 0;
+        yy = 3;
+        sw1 = false;
+        hit = hits;
+        life = lifes;
+        score = scores;
+        active = false;
+    }
+
+    char getKey() {
         return key;
     }
-	int gethit(){
-		return hit;
-	}
-	int getlife(){
-		return life;
-	}
-	int getscore(){
-		return score;
-	}
-	airplane(){
-		i=0;
-		k=0;
-		gy=0;
-		yy=3;
-		sw1=false;
-		hit=hits;
-		life=lifes;
-		score=scores;
-		active=false;
 
-	}
-	void gotoxy(int x, int y){
-	
-	COORD pos = {x-1, y-1};
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),pos);
-
-}
-void delay(int x){
-	Sleep(x);
-}
-void scr_mov()
-{
-	gotoxy(x,y);
-	cout<<face<<endl;
-}
-void scr_clear() // √ﬂ∞°
-{
-    gotoxy(x,y);
-    cout<<"          "<<endl;
-}
-void  scr_mov2()
-{	
-	
-    for(i=0;i<5;i++){
-        gotoxy(bx[i],by[i]);
-        cout<<bad1[i]<<endl;
+    int gethit() {
+        return hit;
     }
-	delay(20);
-    // ¿Ã¿¸ø° √‚∑¬µ» ¿˚ ∫Ò«‡±‚ ¿ÃπÃ¡ˆ ¡ˆøÏ±‚
-    for(i=0;i<5;i++){
-        gotoxy(bx2[i],by2[i]);
-        cout<<"       "<<endl;
+
+    int getlife() {
+        return life;
     }
-    // «ˆ¿Á √‚∑¬µ» ¿˚ ∫Ò«‡±‚ ¿ÃπÃ¡ˆ∏¶ ¿Ã¿¸ ¿ßƒ° πËø≠ø° ∫πªÁ
-   for(i=0;i<5;i++){
-        bx2[i] = bx[i];
-        by2[i] = by[i];
-    }	
-   for(i=0;i<5;i++){
-	   if(y<=by[i]){
-			scr_mov();
-			strcpy(bad1[i],"°ﬂ°È°ﬂ");
-	   }
-   }
-}
-void  hit_fun(){
-		system("cls");
-		gotoxy(26,8);
-		cout<<"∏Òº˚"<<life<<"≥≤¿Ω"<<endl;
-		gotoxy(26,10);
-		cout<<"πÃº« º∫∞¯"<<endl;
-}
-void  life_fun(){
-	system("cls");
-	gotoxy(26,8);
-	cout<<"«ˆ¿Á ¿˚±∫¿∫ "<<hit<<"∏∂∏Æ ªÏæ∆¿÷Ω¿¥œ¥Ÿ.";
-	gotoxy(26,10);
-	cout<<"πÃº« Ω«∆–"<<endl;
-}
-void  start()
-{
-	
-	gotoxy(21,8);
-	cout<<"¡÷¿Œ∞¯ ¿Ãµø : ¡¬øÏ »≠ªÏ«• ≈∞∫∏µÂ"<<endl;
-	gotoxy(26,10);
-	cout<<"π´±‚ πﬂªÁ : Ω∫∆‰¿ÃΩ∫πŸ"<<endl;
-	gotoxy(20,12);
-	cout<<"S ≈∞∏¶ º±≈√«œ∏È ∞‘¿”¿Ã Ω√¿€µÀ¥œ¥Ÿ."<<endl;
-	cin>>key;
-}
-void  gun_mov()
-{
-	
-	gotoxy(gx,gy);
-	cout<<"°„"<<endl;
-	gotoxy(gx,gy+1);
-		cout<<"  "<<endl;
-		if(gy==1){
-			gotoxy(gx,1);
-			cout<<"  "<<endl;
-		
-		}
-		scr_crash();
 
-		
-}
-void  scr_move(){
-	gotoxy(4,1);
-		cout<<"hit : "<<hit<<" "; // º˝¿⁄ 10ø°º≠ 1¿ª ª©∏È 90¿∏∑Œ ∫∏¿”
-		gotoxy(16,1);
-		cout<<"life : "<<life;
-	if(kbhit()){
-			key_in=getch();
-			if(key_in == LEFT){
-				if(x>2){
-					scr_clear();
-					x--;
-				}
-			}else if(key_in == RIGHT ){
-				if(x<70){
-					scr_clear();
-					x++;
-				}
-			}else if(key_in == ' '){
-				if(gy==0){
-					sw1=true;
-					gx=x + 3;
-					gy=23;
+    int getscore() {
+        return score;
+    }
 
-				}
-			}
-	}
-		
-	if(sw1==true){
-		gy--;
-		if(gy==0){
-			sw1=false;
-		}else{
-			gun_mov();
-		}
-	}
-	if(life==3||yy==24){
-			active=true;
-	}
-	if(yy==24){
-			yy=3;
-		}else{
-			if(k==10){
-				k=0;
-				yy++;
+    void pstr_cpy() {
+        str_cpy();
+    }
 
-			}else{
-				k++;
-			}
-		}
-		for(i=0;i<5;i++){
-			by[i]=yy;	
-		}
-		if(active==true){
-			crash1();
-		}
-		scr_mov2();
-}
+    void pstart() {
+        start();
+    }
 
-void  str_cpy(){
-	char st1[15]="¢ƒ-°„-¢≈";
-	char st2[10]="°ﬂ°È°ﬂ";
-	char st3[10]="      ";
-	strcpy(face,st1);
-	x=35;
-	y=22;
-	
-	for(int i=0; i<5; i++){
-		strcpy(bad1[i],st2); // iπ¯¬∞ πËø≠ø° πÆ¿⁄ø≠ ∫πªÁ
-		bx[i]=(i+1)*13;
-		by[i]=3;
-	}
-}
-void  scr_crash(){
-	for(int i=0;i<5;i++){
-		if(gx>bx[i]-1&&gx<bx[i]+11&&gy==by[i]){
-			if(strcmp(bad1[i],"       ")==1){
-				strcpy(bad1[i],"       ");
-				//strcpy();
-				hit=hit-1;
-			}
-		}	
-	}
-	
-}
-void  crash1(){
-	for(int i=0;i<5;i++){
-		//if(x>bx[i]+6&&x+8<bx[i]&&y-1==by[i]){
-		if(x<bx[i]+6&&x+8>bx[i]&&y-1==by[i]){
-			if(strcmp(bad1[i],"       ")==1){
-					life=life-1;
-					active=false;	
-					
-			}
+    void pscr_mov() {
+        scr_mov();
+    }
 
-		}
-				//hero.y-1ø°º≠ ¡∂∞«¿ª ¡‡º≠ «ÿ∞·¡ô¥Ÿ
-	}
+    void pscr_move() {
+        scr_move();
+    }
 
-}
+    void phit_fun() {
+        hit_fun();
+    }
+
+    void pgotoxy(int x, int y) {
+        gotoxy(x, y);
+    }
+
+    void plife_fun() {
+        life_fun();
+    }
+
+    void pdelay(int x) {
+        delay(x);
+    }
+
+    void runGame() {
+        time_t start, finish;
+        double duration;
+
+        pstr_cpy();
+        start = time(NULL);
+        pstart();
+        if (getKey() == 's') {
+            system("cls");
+            while (1) {
+                pscr_mov();
+                pscr_move();
+
+                pdelay(10);
+
+                if (gethit() == 0) {
+                    phit_fun();
+                    finish = time(NULL);
+                    duration = (double)(finish - start);
+                    pgotoxy(28, 1);
+                    cout << "time : " << duration << "Ï¥à";
+                    break;
+                }
+                else if (getlife() == 0 || getscore() == 0) {
+                    plife_fun();
+                    break;
+                }
+            }
+        }
+    }
 };
 
-
-
-int main(){
-	time_t start, finish;
-	double duration;
-	airplane hero;
-
-	hero.str_cpy();
-	start = time(NULL);
-	hero.start();
-	 if (hero.getKey() == 's') {
-		system("cls");
-	while(1)
-	{
-		hero.scr_mov();
-		hero.scr_move();
-		
-		
-		delay(10);
-
-
-		if(hero.gethit()==0){
-			
-			hero.hit_fun();
-			finish = time(NULL);
-			duration = (double)(finish - start);
-			gotoxy(28,1);
-			cout<<"time : "<<duration<<"√ ";
-			break;
-		}else if(hero.getlife() ==0 || hero.getscore() ==0){
-			hero.life_fun();
-			break;
-		}
-	}
-	}
-	return 0;
+int main() {
+    airplane hero;
+    hero.runGame();
+    return 0;
 }
